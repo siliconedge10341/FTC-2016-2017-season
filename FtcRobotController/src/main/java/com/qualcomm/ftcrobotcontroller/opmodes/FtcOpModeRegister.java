fd1d7d134358335e3af1f3125ec74fa0c25aec30
iamthedival <bandiv2011@gmail.com>
2016-09-30 21:03:05 -0400

    We changed the hardware map inside pushbothardware

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -59,6 +59,7 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("DemoOP", DemoOp.class);
+    manager.register("DemoAutonomous", DemoAutonomous.class);
    // manager.register("K9Line", K9Line.class);
     //manager.register ("PushBotAuto", PushBotAuto.class);
    // manager.register ("PushBotManual", PushBotManual.class);
