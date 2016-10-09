acbc4316e46b17ff8fd08833ac434117f27dc3a0
iamthedival <bandiv2011@gmail.com>
2016-10-09 16:15:07 -0400

    Made varialbes private in the class

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Mecanum.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Mecanum.java
@@ -6,15 +6,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by Dival Banerjee on 9/7/2016.
+ * Created by the lord and savior Arko Chaterjee on 9/7/2016.
  */
 public class Mecanum{
 
-    float FRpower;
-    float FLpower;
-    float BRpower;
-    float BLpower;
-
+    private float FRpower;
+    private float FLpower;
+    private float BRpower;
+    private float BLpower;
+//Private variables
 
     public Mecanum() {
 
@@ -37,6 +37,7 @@ public class Mecanum{
         float ch3 = ljoystick_y;
         float ch4 = ljoystick_x;
 
+        //Get joystick inputs
         FLpower = ch3 + ch1 + ch4;
         BLpower = ch3 + ch1 - ch4;
         FRpower = ch3 - ch1 - ch4;
@@ -50,33 +51,6 @@ public class Mecanum{
     }
     //initialization routine
 
-    double scaleInput(double dVal)  {
-        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
 
-        // get the corresponding index for the scaleInput array.
-        int index = (int) (dVal * 16.0);
-
-        // index should be positive.
-        if (index < 0) {
-            index = -index;
-        }
-
-        // index cannot exceed size of array minus 1.
-        if (index > 16) {
-            index = 16;
-        }
-
-        // get value from the array.
-        double dScale = 0.0;
-        if (dVal < 0) {
-            dScale = -scaleArray[index];
-        } else {
-            dScale = scaleArray[index];
-        }
-
-        // return scaled value.
-        return dScale;
-    }
 
 }
