98b7560758beb689d9acd2f2981e420f8b9c5608
Evan <evankrimp@gmail.com>
2016-12-31 17:47:00 -0500

    ---FIXED--- Krimp's: init is done.
    ---ADDED---
    none.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Krimp_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Krimp_Autonomous.java
@@ -5,7 +5,6 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.classes.Light;
@@ -39,14 +38,15 @@ public class Krimp_Autonomous extends VisionOpMode {
         private DcMotor mtrBR;
         private DcMotor mtrBL;
         private DcMotor mtrShootR;
-        private DcMotor MtrShootL;
+        private DcMotor mtrShootL;
 
     // Servos
         private Servo srvRelease;
 
     // Classes
         private Mecanum drive_train = new Mecanum();
-        private Range range_sensor = new Range();
+        private Range range_sensor_beacon = new Range();
+        private Range range_sensor_proj = new Range();
         private Light color_sensor = new Light();
         private ElapsedTime runtime = new ElapsedTime();
 
@@ -75,13 +75,47 @@ public class Krimp_Autonomous extends VisionOpMode {
         //
         // Initializes every motor, servo, variable, and position.
         //
+        v_state = 0;
+        initialC = 0;
+        initialD = 0;
+
         // Motors
+        mtrFR = hardwareMap.dcMotor.get("fr_motor");
+        mtrFL = hardwareMap.dcMotor.get("fl_motor");
+        mtrBR = hardwareMap.dcMotor.get("br_motor");
+        mtrBL = hardwareMap.dcMotor.get("bl_motor");
+        mtrShootR = hardwareMap.dcMotor.get("shooter_right");
+        mtrShootL = hardwareMap.dcMotor.get("shooter_left");
 
         // Servos
+        srvRelease = hardwareMap.servo.get("servo_ball");
 
         // Classes
+        color_sensor.setV_sensor(hardwareMap.opticalDistanceSensor.get("ods_line"));
+        range_sensor_beacon.setRange(hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range_beacon"));
+        range_sensor_proj.setRange(hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range_proj"));
 
+        // Positions
+        srvRelease.setPosition(0.3);
 
+        super.init();
+        this.setCamera(Cameras.PRIMARY);
+        this.setFrameSize(new Size(900, 900));
+
+        enableExtension(VisionOpMode.Extensions.BEACON);         // Beacon detection.
+        enableExtension(VisionOpMode.Extensions.ROTATION);       // Automatic screen rotation correction.
+        enableExtension(VisionOpMode.Extensions.CAMERA_CONTROL); // Manual camera control.
+        beacon.setAnalysisMethod(Beacon.AnalysisMethod.FAST);
+
+        beacon.setColorToleranceRed(0);
+        beacon.setColorToleranceBlue(0);
+
+        rotation.setIsUsingSecondaryCamera(false);
+        rotation.disableAutoRotate();
+        rotation.setActivityOrientationFixed(ScreenOrientation.PORTRAIT);
+
+        cameraControl.setColorTemperature(CameraControlExtension.ColorTemperature.AUTO);
+        cameraControl.setAutoExposureCompensation();
     }
 
     // Start
