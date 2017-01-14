151084b2d79dcbda1bf5fe0dc97bfe11e0647cae
Evan <evankrimp@gmail.com>
2017-01-13 21:09:48 -0500

    ---FIXED--- Mecanum: added comments and fixed math Color Sensor Driver: fixed package.
    ---ADDED---
    none

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/Mecanum.java
@@ -1,30 +1,23 @@
 package org.firstinspires.ftc.teamcode.classes;
 
-import android.preference.Preference;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorController;
-//import com.qualcomm.robotcore.hardware.DcMotorControllerEx;
-//import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by the lord and savior Arko Chaterjee on 9/7/2016.
+ * Created by the lord and savior Arko Chaterjee, Richard the genius, and Krimps on 9/7/2016.
  */
 public class Mecanum{
     // instance variables
     // private variables
-    // constants
-    private final double pi = 3.1415926;
-    private final double wheel_diameter = 4 * (pi);
-    private Light dis_sensor = new Light();
+        // constants
+        private final double pi = 3.1415926;
+        private final double wheel_diameter = 4 * (pi);
 
-    // powers
-    private double FRpower = 0;
-    private double FLpower = 0;
-    private double BRpower = 0;
-    private double BLpower = 0;
-    private double BasePower ;
-    //private double MaxPower = 1;
+        // powers
+        private double FRpower = 0;
+        private double FLpower = 0;
+        private double BRpower = 0;
+        private double BLpower = 0;
+        private double BasePower = 0;
 
     // constructors
     public Mecanum() {
@@ -33,25 +26,10 @@ public class Mecanum{
         FLpower = 0;
         BRpower = 0;
         BLpower = 0;
-        BasePower = 0;
-    }
-    public Mecanum(double motorFR, double motorFL, double motorBR, double motorBL) {
-        // fill constructor
-        // this sets the motors to the specific train withing demo and drive
-        FRpower = motorFR;
-        FLpower = motorFL;
-        BRpower = motorBR;
-        BLpower = motorBL;
         BasePower = .5;
     }
 
     // sets
-    public void run_motor(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
-        motorFR.setPower(FRpower);
-        motorBR.setPower(BRpower);
-        motorFL.setPower(FLpower);
-        motorBL.setPower(BLpower);
-    }
     public void set_Power(float rjoystick_x,float ljoystick_y,float ljoystick_x, boolean percision_flag){
         float ch1 = rjoystick_x;
         float ch3 = ljoystick_x;
@@ -59,46 +37,43 @@ public class Mecanum{
 
         boolean FL = ch4 > ch3;
         boolean FR = ch4 > -ch3;
-        if(ch1 < -0.4 || ch1 > 0.4)
-        {
+        if (ch1 < -0.4 || ch1 > 0.4) {
             FLpower = ch1;
             BLpower = ch1;
             FRpower = ch1;
             BRpower = ch1;
-        }
-        else if(FL && FR)
-        {
+        } else if (FL && FR) {
             FLpower = -ch4;
             BLpower = -ch4;
             FRpower = ch4;
             BRpower = ch4;
-        }
-        else if(FL || FR)
-        {
-            if(FL)
-            {
-                FLpower = -ch3;  //0.7
+        } else if (FL || FR) {
+            if(FL) {
+                //
+                // Will be adjusted
+                //
+                FLpower = -ch3; //0.7
                 BLpower = ch3;  //0.95
                 FRpower = -ch3; //0.78
-                BRpower = ch3;       //1
-            }
-            else
-            {
+                BRpower = ch3;  //1
+            } else {
                 FLpower = -ch3; //.78
-                BLpower = ch3;       //1
-                FRpower = -ch3;  //0.7
+                BLpower = ch3;  //1
+                FRpower = -ch3; //0.7
                 BRpower = ch3;  //0.95
             }
-        }
-        else
-        {
+        } else {
             FLpower = -ch4;
             BLpower = -ch4;
             FRpower = ch4;
             BRpower = ch4;
         }
-
-        if(Math.abs(FLpower) + Math.abs(BLpower) + Math.abs(FRpower) + Math.abs(BRpower) < 1)
+        //
+        // Power has to be over a certain amount. We found this to work because the amount of
+        // power that is below 1 together doesn't move the robot anyways. So, in order to protect
+        // the motors, we did this in case.
+        //
+        if (Math.abs(FLpower) + Math.abs(BLpower) + Math.abs(FRpower) + Math.abs(BRpower) < 1)
         {
             FLpower = 0;
             BLpower = 0;
@@ -106,7 +81,7 @@ public class Mecanum{
             BRpower = 0;
         }
 
-/*
+        /*
         double X2 = 0, Y1 = 0, X1 = 0, threshold = .15;
 
         if(Math.abs(ch3) > threshold)
@@ -127,6 +102,7 @@ public class Mecanum{
         if(Y1<.3 && Y1>-.3){
             Y1 = 0;
         }
+
         if (percision_flag = true) {
             FLpower = -(Y1 + X2 + X1);
             BLpower = (Y1 + X2 - X1);
@@ -138,17 +114,22 @@ public class Mecanum{
             FRpower = -(Y1 - X2 - X1) / 2;
             BRpower = (Y1 - X2 + X1) / 2;
         }
-*/
+        */
 
     }
     public void setPosition(int fr,int fl,int br, int bl, DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL){
+        //
+        // Sets the motor position to something and the runs it.
         motorFR.setTargetPosition(fr);
         motorFL.setTargetPosition(fl);
         motorBR.setTargetPosition(br);
         motorBL.setTargetPosition(bl);
+
     }
     public void setPowerD(double power) {
-
+        //
+        // Sets the power of the motor
+        //
         BasePower = power;
 
     }
@@ -164,25 +145,47 @@ public class Mecanum{
         return wheel_rotation;
     }
 
-    public void run_left_using_alignment(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL, double distancefront, double distanceback)
-    {
-        FLpower = -BasePower;
-        BLpower = BasePower; //0.95
-        FRpower = -BasePower;  //0.73
-        BRpower = BasePower; // 1
-        double distancedifference = distancefront - distanceback;
-        if(distancedifference > 1)
-        {
-            //turn right
-            BLpower += (double) (distancedifference/60);
-            BRpower += (double) (distancedifference/60);
+    // methods
+    public void run_motor(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
+        motorFR.setPower(FRpower);
+        motorBR.setPower(BRpower);
+        motorFL.setPower(FLpower);
+        motorBL.setPower(BLpower);
+    }
+
+        // testers
+        public int testDistance(DcMotor A_Motor) {
+            // This takes any motor and tests the distance that that motor has traveled
+            // We only do one because all have traveled the same amount. Knowing Turns
+            // Do not count.
+
+            if (Math.abs(A_Motor.getCurrentPosition()) >= Math.abs(A_Motor.getTargetPosition())){
+                return 1;
+            }else{
+                return 0;
+            }
         }
-        else if(distancedifference < -1)
-        {
-            distancedifference = -1 * distancedifference;
-            //turn left
-            FLpower -= (double) (distancedifference/60);
-            FRpower -= (double) (distancedifference/60);
+
+    // directions for the robot to run
+    public void run_left_using_alignment(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL, double distanceFrontR, double distanceBackR) {
+        FLpower = -BasePower;
+        BLpower = BasePower;
+        FRpower = -BasePower;
+        BRpower = BasePower;
+        double distanceDifference = distanceFrontR - distanceBackR;
+        if(distanceDifference > 1) {
+            //
+            // turn right
+            //
+            BLpower += distanceDifference/60;
+            BRpower += distanceDifference/60;
+        } else if(distanceDifference < -1) {
+            distanceDifference = -1 * distanceDifference;
+            //
+            // turn left
+            //
+            FLpower -= distanceDifference/60;
+            FRpower -= distanceDifference/60;
         }
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
@@ -190,149 +193,164 @@ public class Mecanum{
         motorBL.setPower(BLpower);
     }
 
-    public void run_right_using_alignment(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL, double distancefront, double distanceback)
-    {
+    public void run_right_using_alignment(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL, double distanceFrontR, double distanceBackR) {
         FLpower = BasePower;
-        BLpower = -BasePower; //0.95
-        FRpower = BasePower;  //0.73
-        BRpower = -BasePower; // 1
-        double distancedifference = distancefront - distanceback;
-        if(distancedifference > 1)
-        {
-            //turn right
-            FLpower += (double) (distancedifference/60);
-            FRpower += (double) (distancedifference/60);
-        }
-        else if(distancedifference < -1)
-        {
-            distancedifference = -1 * distancedifference;
-            //turn left
-            BLpower -= (double) (distancedifference/60);
-            BRpower -= (double) (distancedifference/60);
+        BLpower = -BasePower;
+        FRpower = BasePower;
+        BRpower = -BasePower;
+        double distanceDifference = distanceFrontR - distanceBackR;
+        if(distanceDifference > 1) {
+            //
+            // turn right
+            //
+            FLpower += distanceDifference/60;
+            FRpower += distanceDifference/60;
+        } else if(distanceDifference < -1) {
+            distanceDifference = -1 * distanceDifference;
+            //
+            // turn left
+            //
+            BLpower -= distanceDifference/60;
+            BRpower -= distanceDifference/60;
         }
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+
     }
 
     // methods
+    //
     // These are the functions for the specific direction
+    //
     public void run_left(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
-        FLpower = -BasePower;  //0.7
-        BLpower = BasePower; //0.95
-        FRpower = -BasePower;  //0.73
-        BRpower = BasePower; // 1
+        FLpower = -BasePower;
+        BLpower = BasePower;
+        FRpower = -BasePower;
+        BRpower = BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_right(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR , DcMotor motorBL) {
-        FLpower = BasePower; //.73
-        BLpower = -BasePower;       //1
-        FRpower = BasePower;  //.7
-        BRpower = -BasePower;  //.95
+        FLpower = BasePower;
+        BLpower = -BasePower;
+        FRpower = BasePower;
+        BRpower = -BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_forward(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = BasePower;
         BLpower = BasePower;
         FRpower = -BasePower;
         BRpower = -BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_backward(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = -BasePower;
         BLpower = -BasePower;
         FRpower = BasePower;
         BRpower = BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_diagonal_right_up(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = BasePower;
         BLpower = 0;
         FRpower = 0;
         BRpower = BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
-
+        //
         motorFR.setPower(-FRpower);
         motorBR.setPower(-BRpower);
         motorFL.setPower((FLpower));
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_diagonal_left_up(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = BasePower;
         BLpower = 0;
         FRpower = 0;
         BRpower = -BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_diagonal_left_down(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = -BasePower;
         BLpower = 0;
         FRpower = 0;
         BRpower = -BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
+        //
         motorFR.setPower(-FRpower);
         motorBR.setPower(-BRpower);
         motorFL.setPower((FLpower));
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void run_diagonal_right_down(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = 0;
         BLpower = -BasePower;
         FRpower = -BasePower;
         BRpower = 0;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
-
+        //
         motorFR.setPower(-FRpower);
         motorBR.setPower(-BRpower);
         motorFL.setPower((FLpower));
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
     public void turn_right(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
         FLpower = BasePower;
@@ -351,8 +369,10 @@ public class Mecanum{
         BLpower = -BasePower;
         FRpower = -BasePower;
         BRpower = -BasePower;
+        //
         // The positive and negative inputs only mean direction, not speed.
         // This sets the motors that go into DemoAutonomous.
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
@@ -363,59 +383,54 @@ public class Mecanum{
         BLpower = 0;
         FRpower = 0;
         BRpower = 0;
+        //
         // The positive and negative inputs only mean direction, not speed.
-
+        //
         motorFR.setPower(FRpower);
         motorBR.setPower(BRpower);
         motorFL.setPower(FLpower);
         motorBL.setPower(BLpower);
+        //
         // This sets the motors that go into DemoAutonomous.
-
+        //
     }
 
     // resets
     public void reset_encoders(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
+        //
         // The positive and negative inputs only mean direction, not speed.
+        //
         motorFR.setMode(DcMotor.RunMode.RESET_ENCODERS);
         motorBR.setMode(DcMotor.RunMode.RESET_ENCODERS);
         motorFL.setMode(DcMotor.RunMode.RESET_ENCODERS);
         motorBL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
+        //
         // This sets the motors that go into DemoAutonomous.
+        //
     }
     public void run_to_position(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
+        //
         // The positive and negative inputs only mean direction, not speed.
+        //
         motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
+        //
         // This sets the motors that go into DemoAutonomous.
+        //
     }
     public void run_using_encoders(DcMotor motorFR, DcMotor motorFL, DcMotor motorBR, DcMotor motorBL) {
+        //
         // The positive and negative inputs only mean direction, not speed.
+        //
         motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
         motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
         motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
         motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
-
+        //
         // This sets the motors that go into DemoAutonomous.
+        //
     }
-
-    // testers
-    public int testDistance(DcMotor A_Motor) {
-        // This takes any motor and tests the distance that that motor has traveled
-        // We only do one because all have traveled the same amount. Knowing Turns
-        // Do not count.
-
-        if (Math.abs(A_Motor.getCurrentPosition()) >= Math.abs(A_Motor.getTargetPosition())){
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-    //initialization routine
-
-
 
 }
